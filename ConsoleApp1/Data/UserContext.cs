5733a8b58312c81f8c1896b61525d76c4bf4fc87
camalzade_elvin <camalzade_elvin@outlook.com>
2019-02-14 16:40:55 +0400

    Add project files.

---
+++ ConsoleApp1/Data/UserContext.cs
@@ -0,0 +1,18 @@
+﻿using ConsoleApp1.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Data
+{
+    class UserContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public UserContext() : base("UserDb")
+        {           
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using ConsoleApp1.Data;
+using ConsoleApp1.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User() {
+                Email="camalzade1@gmail.com",
+                Password="123456"
+            };
+
+            UserContext userContext = new UserContext();
+            userContext.Users.Add(user);
+        }
+    }
+}
